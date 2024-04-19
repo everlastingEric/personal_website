46727b273b37b5e8bcb00cf020877f95dfa5f897
StemPunks9254 <ericshen567890@gmail.com>
2024-04-18 23:35:51 -0700

    started home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import Link from 'next/link'
+import Image from 'next/image';
+import introPic from './components/images/intro.jpg';
+
 
 export default function Page() {
     return (
-        <div className="container mx-auto px-4">
-            <h1>About Me</h1>
-            <div className="w-full h-screen">
+        <div className="container mx-auto">
+            <div className="w-full flex flex-wrap">
+                <div className="md:flex md:flex-row pt-5">
+                    <Image
+                        src={introPic}
+                        width={350}
+                        height={350}
+                        alt="picture of me"
+                        className="flex object-scale-down py-3.5 px-4" />
+                    <p className="py-3.5 px-24 lg:py-24 indent-8 text-xl">
+                        Hi, my name is Eric Shen, welcome to my website! This website will serve as an extended version of my resume where you can learn more about me, my experiences, and the projects I have undertaken. Feel free to explore this website by clicking on any of the links you see, thank you for visiting!
+                    </p>
+                </div>
+                <div className="text-4xl">About me:</div>
                 <p>
-                    More information coming soon...
+
                 </p>
             </div>
         </div>
-    )
+    );
 }
